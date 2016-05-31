dc0487c42d3d2da97e0412b3079c55345c5aa0fe
Alexey Bondarenko <alexeybondarenko@me.com>
2016-06-01 01:38:54 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -237,7 +237,7 @@ gulp.task('watch', function () {
 // Deploy gh-pages
 gulp.task('deploy-prefix', function () {
   return gulp.src('./www/**/*.html')
-    .pipe(prefix('/forza-web'))
+    .pipe(prefix('/simple-webapp'))
     .pipe(gulp.dest('./www'));
 });
 
